baea081a92e5e6e2c94c97d49795fa434b402f57
martinmen <martinhmsuni@gmail.com>
2021-07-28 13:28:11 -0300

    Initial commit

---
+++ app/src/main/java/ar/com/unlam/itercorpappchallenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package ar.com.unlam.itercorpappchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
